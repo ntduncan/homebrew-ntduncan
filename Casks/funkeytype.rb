3fa23775671e993a16f7b23931fa7866e9b36fd1
Nate Duncan <60360613+ntduncan@users.noreply.github.com>
2025-07-03 22:01:23 -0600

    funkeytype v1.0.0

--- Casks/funkeytype.rb
+++ Casks/funkeytype.rb
@@ -1,20 +1,13 @@
-class Funkeytype < Formula
+  cask "funkeytype" do
+  version "1.0.0"
+  
+    sha256 "ac1374be318c25696c0d0dabf035c46831c81ca5486682b0a05fba836ef8dfe1"
+  url "https://github.com/ntduncan/funkeytype/releases/download/v#{version}/funkeytype-darwin-universal.tar.gz"
+  
+  name "Funkeytype"
   desc "Typing test in your terminal"
   homepage "https://github.com/ntduncan/funkeytype"
-  version "1.0.0"
-  url "https://github.com/ntduncan/funkeytype/archive/v1.0.0.tar.gz" 
-  sha256 "20d696628da9c212e848246b1ea0aec2b72861ab179de0fa47dc2e000c1d287" 
-  license "MIT"
-
-  depends_on "go" => :build
-
-  def install
-    system "go", "build", "-o", "funkeytype", "."
-    bin.install "funkeytype"
-  end
-
-  test do
-    # This is a basic test, you can expand it
-    assert_match "funkeytype version", shell_output("#{bin}/funkeytype --version")
-  end
+  
+  binary "funkeytype"
 end
+
